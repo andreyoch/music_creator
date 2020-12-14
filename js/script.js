dc7882a4b34464d57f7ac0e1b5b266a34707f957
Andrew T <at100991@student.sgh.waw.pl>
2020-12-14 08:17:47 +0200

    -mAdd header

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-document.addEventListener('DOMContentLoaded',main);
+document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-   document.addEventListener('keypress', (e) => playAudio(e));
-
+   document.addEventListener('keypress', (e) => {
+      playAudio(e)
+      showPlayingClass(e);
+   });
+   
 }
 
 function playAudio(e) {
-   console.log(e.key)
-   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-   console.log
-   if (audio) {
-      audio.play();
+  const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+  if (audio) {
+    audio.play();
+  }
+}
+function showPlayingClass(e) {
+   const element = document.querySelector(`div[data-key="${e.key}"]`);
+   element.classList.add('playing');
+   setTimeout(removeClass, 300, element, 'playing');
+   
+   function removeClass(element,className) {
+      element.classList.remove(className)
    }
-   }
+}
+
+
